1c24bb067a3e281e63299f688ff416dbaec2254e
vaguilar25 <vivian.aguilar@gmail.com>
2018-11-19 17:54:31 -0600

    get things work

--- javascript/script.js
+++ javascript/script.js
@@ -1,43 +1,48 @@
-$(document).ready(function(){
+var numrecords = 1;
+
+$(document).on("click", "#run-search", function () {
+
     var apiKey = "4884a5de92f24947b5a32839f9a49e4e";
-    var searchString = "Steph Curry";
-//    var searchString = $("....").val();
-//    var numrecords = $("....").val();
-//    var startYear = $("....").val() + "0101";
-//    var endYear = $("....").val() + "1231";
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key="+apiKey+
-        "&q="+searchString;
-//        "&begin_date="+startYear+
-//        "&end_date="endYear;
+
+    var searchString = $("#search-term").val();
+    numrecords = $("#num-records-select").val();
+    var startYear = $("#start-year").val() + "0101";
+    var endYear = $("#end-year").val() + "1231";
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + apiKey +
+        "&q=" + searchString;
+    //  "&begin_date=" + startYear +
+    //      "&end_date=" + endYear;
+    console.log(queryURL);
+
     $.ajax({
         url: queryURL,
-        method: "GET",
-    }).then(function(response){
+        method: "GET"
+    }).then(function (response) {
         console.log(response);
         console.log(response.response.docs[0].headline.main);
         var getData = response.response.docs;
         console.log(response.response.docs);
-       generateArticles(response.response.docs);
-//        var getParentDiv = $(".card");
-//    var ChildList = $(".ul");
-//    ChildList.addClass("list-group list-group-flush");
-//    console.log("inside generate article");
-//    console.log(getData.length);
-//    console.log(getData[0]);
-//  console.log("getData[0] -" ,getData[0]);
-//   for(i=0;i<getData.length;i++){
-//       var addList = $("<li>");
-//       addList.addClass("list-group-item");
-//       console.log("inside for loop");
-//       addList.text(getData[i].headline.main);
-//       ChildList.append(addList);
-//       console.log(getData[i]);
-//       console.log(getData[i].headline.main);
-//   }
-    })
-    
-    
-})
+        generateArticles(response.response.docs);
+        //        var getParentDiv = $(".card");
+        //    var ChildList = $(".ul");
+        //    ChildList.addClass("list-group list-group-flush");
+        //    console.log("inside generate article");
+        //    console.log(getData.length);
+        //    console.log(getData[0]);
+        //  console.log("getData[0] -" ,getData[0]);
+        //   for(i=0;i<getData.length;i++){
+        //       var addList = $("<li>");
+        //       addList.addClass("list-group-item");
+        //       console.log("inside for loop");
+        //       addList.text(getData[i].headline.main);
+        //       ChildList.append(addList);
+        //       console.log(getData[i]);
+        //       console.log(getData[i].headline.main);
+        //   }
+    });
+
+
+});
 
 
 //var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
@@ -64,24 +69,24 @@ $(document).ready(function(){
 
 
 
-function generateArticles(getData){
+function generateArticles(getData) {
     var getParentDiv = $(".card");
     var ChildList = $("<ul>");
     ChildList.addClass("list-group list-group-flush");
     console.log("inside generate article");
     console.log(getData.length);
     console.log(getData[0]);
-  console.log("getData[0] -" ,getData[0]);
-   for(i=0;i<getData.length;i++){
-       var addList = $("<li>");
-       addList.css("background-color","#ecf0f1");
-       addList.addClass("list-group-item well");
-       console.log("inside for loop");
-       addList.text(getData[i].headline.main);
-       ChildList.append(addList);
-       console.log(getData[i]);
-       console.log(getData[i].headline.main);
-   }
+    console.log("getData[0] -", getData[0]);
+    for (i = 0; i < numrecords; i++) {
+        var addList = $("<li>");
+        addList.css("background-color", "#ecf0f1");
+        addList.addClass("list-group-item well");
+        console.log("inside for loop");
+        addList.text(getData[i].headline.main);
+        ChildList.append(addList);
+        console.log(getData[i]);
+        console.log(getData[i].headline.main);
+    }
     getParentDiv.append(ChildList);
-    
+
 }
